9ca056c02345f2bc877304f248bcbd8ab4426938
Oleksii <oleksisem@gmail.com>
2025-05-29 14:35:15 -0400

    force exit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
     res.json({ message: 'pong' });
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+let server = null;
+if (require.main === module) {
+    server = app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
 
-module.exports = app; // для тестів
+module.exports = { app, server };
--- index.test.js
+++ index.test.js
@@ -1,9 +1,18 @@
 const request = require('supertest');
-const app = require('./index');
+const { app } = require('./index');
+let server;
+
+beforeAll((done) => {
+    server = app.listen(4000, done); // запускай на іншому порту, щоб не було конфлікту
+});
+
+afterAll((done) => {
+    server.close(done); // закрий сервер після всіх тестів
+});
 
 describe('GET /ping', () => {
     it('responds with pong', async () => {
-        const res = await request(app).get('/ping');
+        const res = await request(server).get('/ping');
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('message', 'pong');
     });
